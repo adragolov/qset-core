95a5fe75f1471c920f77a829a50b64b791e73dff
Atanas Dragolov <adragolov@gmail.com>
2020-04-23 22:11:58 +0300

    Integrate versioneer.

--- setup.py
+++ setup.py
@@ -1,18 +1,20 @@
 import setuptools
+import versioneer
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 
 setuptools.setup(
-    name="qset-core-adragolov", # Replace with your own username
-    version="1.0.0",
+    name="qset-core-adragolov",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
     author="Atanas Dragolov",
     author_email="adragolov@gmail.com",
     description="Core redistributable of the Quantset toolbox.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/adragolov/qset-core",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
