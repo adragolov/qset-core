0172d195faa3bd63aa3a66e58614e8de7ff49910
Atanas Dragolov <adragolov@gmail.com>
2020-04-23 21:44:40 +0300

    Adding setup.cfg

---
+++ qset_core/__init__.py
@@ -0,0 +1,9 @@
+from qset_core.logging import setup_logging
+from qset_core.configuration import ConfigManager, Environment, \
+    ENVIRONMENT_NAME_PRODUCTION, \
+    ENVIRONMENT_NAME_DEFAULT, \
+    ENVIRONMENT_VAR_NAME
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
