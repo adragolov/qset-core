bc9dc899323d32ea9ae7717d1c5f5e0720b6e05d
Atanas Dragolov <adragolov@gmail.com>
2020-04-23 23:39:45 +0300

    Versioneer changes.

--- qset_core/__init__.py
+++ qset_core/__init__.py
@@ -7,3 +7,6 @@ from qset_core.configuration import ConfigManager, Environment, \
 from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
