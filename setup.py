41efb55614e098240bccfb9fb4133daf64d5e379
Atanas Dragolov <adragolov@gmail.com>
2020-04-23 22:03:47 +0300

    Script refactoring

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name="qset-core-adragolov", # Replace with your own username
+    version="1.0.0",
+    author="Atanas Dragolov",
+    author_email="adragolov@gmail.com",
+    description="Core redistributable of the Quantset toolbox.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
